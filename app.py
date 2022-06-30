e1ce130c2acbde1dc50d03caf759c5274c4f11d1
Jennifer Anno-Kusi <jannokusi@gmail.com>
2022-06-30 18:36:56 -0400

    Updating app.py file

--- app.py
+++ app.py
@@ -24,5 +24,12 @@ def welcome():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
 
