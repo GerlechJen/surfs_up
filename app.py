719f5b0a4b45be2374118fb134ab81058c06f84f
Jennifer Anno-Kusi <jannokusi@gmail.com>
2022-06-30 18:47:19 -0400

    Adding updated code

--- app.py
+++ app.py
@@ -36,5 +36,14 @@ def stations():
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
     return jsonify(stations=stations)
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+    
 
 
