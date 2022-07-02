41376a8bde8b246fb60d5e38e51335a76139b103
Jennifer Anno-Kusi <102351522+GerlechJen@users.noreply.github.com>
2022-07-02 18:13:17 -0400

    Uploading app.py file

--- app.py
+++ app.py
@@ -44,6 +44,19 @@ def temp_monthly():
         filter(Measurement.date >= prev_year).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
-    
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps = temps)
 
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps = temps)
